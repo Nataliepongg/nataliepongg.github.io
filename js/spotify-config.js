46994f28bc7eec84d816fbb76639f3a7c81174ac
Natalie <backup4np1234@gmail.com>
2024-12-20 00:43:15 +0800

    fix redirect uri

--- js/spotify-config.js
+++ js/spotify-config.js
@@ -1,6 +1,6 @@
 // js/spotify-config.js
 const SPOTIFY_CONFIG = {
     CLIENT_ID: '0f53bd47f0fa4835ace083b80e20aa7a',
-    REDIRECT_URI: 'http://localhost:8000/pomodoro.html',
+    REDIRECT_URI: 'https://nataliepongg.github.io/pomodoro.html',
     LOFI_PLAYLIST_URI: 'spotify:playlist:37i9dQZF1DX8Uebhn9wzrS'
 };
